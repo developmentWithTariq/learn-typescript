e9464d5d87f3269d0ac4cb08e42abd663d2b45a3
Tariqthecoder <tariqnawaz15994@gmail.com>
2022-12-25 14:41:28 +0500

    Object Types in Typescript

---
+++ step04_a_ObjectTypes/index.ts
@@ -0,0 +1,102 @@
+
+
+
+// let poetLater = {
+//     name: "tariq",
+//     age: 21
+// }
+
+// let poetLater : {
+//     name: string,
+//     age: number
+// }
+
+// poetLater = {
+//     name: "tariq",
+//     age: 21
+// }
+
+// poetLater = "tariq"
+type PoetLater = {
+    name: string,
+    age: number
+}
+
+let poetLater : PoetLater = {
+    name: "ali",
+    age: 24
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let poetLater = {
+//     name: "tariq",
+//     age: 21
+// }
+
+// // poetLater.name
+
+
+// let poetAfter : {
+//     name: string,
+//     age: number
+// }
+
+// // poetAfter = {
+// //     name: "tariq",
+// //     age: 21
+// // }
+// poetAfter = "tariq  "
+
+// poetAfter.age
